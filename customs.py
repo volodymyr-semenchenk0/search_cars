68c22afca40bec5df198cc06819830eef226d756
volodymyr-semenchenko <semenchenko.volodymyr1118@vu.cdu.edu.ua>
2025-05-03 21:49:01 +0300

    change parser to __next data__

--- customs.py
+++ customs.py
@@ -1,18 +1,38 @@
+import requests
+from datetime import datetime
+
+
+def get_eur_to_uah_rate():
+    try:
+        response = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&json")
+        if response.status_code == 200:
+            data = response.json()
+            return float(data[0]["rate"])
+        else:
+            print("[ERROR] Не вдалося отримати курс євро від НБУ. Статус:", response.status_code)
+            return 41  # fallback
+    except Exception as e:
+        print("[ERROR] Виняток при запиті до НБУ:", e)
+        return 41
+
 
 def calculate_customs(year: int, engine_volume: float, engine_type: str, price_eur: float) -> float:
-    import datetime
-
-    current_year = datetime.datetime.now().year
+    current_year = datetime.now().year
     car_age = max(1, current_year - year)
+    rate = get_eur_to_uah_rate()
 
-    if engine_type.lower() == 'diesel':
+    engine_type = engine_type.lower()
+    if engine_type in ['electricity', 'electric', 'bev', 'e']:
+        return 0.0  # Повністю звільнено
+
+    elif engine_type in ['diesel', 'd']:
         base_rate = 75
-    else:  # бензин або інше
-        base_rate = 50
+    else:
+        base_rate = 50  # Для бензину та інших
 
     excise = base_rate * engine_volume * car_age
-    nds = 0.2 * (price_eur * 41 + excise)  # умовний курс 1 EUR = 41 грн
-    import_duty = 0.1 * (price_eur * 41)
+    import_duty = 0.1 * (price_eur * rate)
+    vat = 0.2 * ((price_eur * rate) + excise + import_duty)
 
-    total = excise + nds + import_duty
+    total = excise + import_duty + vat
     return round(total, 2)
--- parsers/autoscout24_parser.py
+++ parsers/autoscout24_parser.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 import json
 from database.db_manager import save_car_to_db
-from customs import calculate_customs
+from customs import calculate_customs, get_eur_to_uah_rate
 
 BASE_URL = "https://www.autoscout24.com"
 
@@ -33,6 +34,11 @@ def get_details_from_json_script(soup):
 def parse_autoscout24(brand=None, model=None, year_from=None, year_to=None,
                       mileage=None, fuel=None, body=None, priceto=None,
                       transmission=None, drive=None, country_code=None):
+
+    eur_rate = get_eur_to_uah_rate()
+    if eur_rate is None:
+        print("[ERROR] Не вдалося отримати курс євро.")
+        return
 
     params = {
         "sort": "standard",
@@ -92,34 +98,74 @@ def parse_autoscout24(brand=None, model=None, year_from=None, year_to=None,
                 if not link_tag:
                     continue
                 detail_url = BASE_URL + link_tag["href"]
+                details_resp = requests.get(detail_url, headers={"User-Agent": "Mozilla/5.0"})
+                detail_soup = BeautifulSoup(details_resp.text, "html.parser")
 
-                detail_resp = requests.get(detail_url, headers={"User-Agent": "Mozilla/5.0"})
-                detail_soup = BeautifulSoup(detail_resp.text, "html.parser")
-                json_data = get_details_from_json_script(detail_soup)
-                if not json_data or "offers" not in json_data:
+                script_tag = detail_soup.find("script", type="application/json")
+                if not script_tag:
+                    print("[WARNING] JSON __NEXT_DATA__ не знайдено.")
                     continue
 
-                offer = json_data.get("offers")
-                item = offer.get("itemOffered", {})
+                next_data = json.loads(script_tag.string)
+                listing = next_data["props"]["pageProps"]["listingDetails"]
+                vehicle = listing.get("vehicle", {})
 
-                identifier_val = item.get("identifier")
-                brand_val = item.get("manufacturer")
-                model_val = item.get("model")
-                year_val = safe_int(item.get("productionDate", "").split("-")[0])
-                mileage_val = safe_int(item.get("mileageFromOdometer", {}).get("value"))
-                fuel_val = item.get("vehicleEngine", [{}])[0].get("fuelType")
-                price_val = safe_float(offer.get("price"))
-                transmission_val = item.get("vehicleTransmission")
-                drive_val = item.get("driveWheelConfiguration")
-                engine_volume = safe_float(item.get("vehicleEngine", [{}])[0].get("engineDisplacement", {}).get("value"))
-                country_val = offer.get("offeredBy", {}).get("address", {}).get("addressCountry")
-                body_val = item.get("bodyType")
+                identifier_val = listing.get("id")
+                brand_val = vehicle.get("make")
+                model_val = vehicle.get("model")
+                year_val = vehicle.get("firstRegistrationDateRaw")
+                if year_val:
+                    try:
+                        year_val = datetime.strptime(year_val, "%Y-%m-%d").year
+                    except ValueError:
+                        print("[WARN] Неможливо обробити дату:", year_val)
+                mileage_val = safe_int(vehicle.get("mileageInKmRaw"))
+                fuel_val = vehicle.get("fuelCategory", {}).get("formatted")
+                price_val = safe_float(listing.get("prices", {}).get("public", {}).get("priceRaw"))
+                transmission_val = vehicle.get("transmissionType")
+                drive_val = vehicle.get("driveTrain")
+                engine_volume = safe_float(vehicle.get("rawDisplacementInCCM"))
+                country_val = listing.get("location", {}).get("countryCode")
+                body_val = vehicle.get("bodyType")
 
-                if not (brand_val and model_val and year_val and price_val):
+                script_tag = detail_soup.find("script", type="application/ld+json")
+                if not script_tag:
+                    print("[WARNING] JSON не знайдено.")
                     continue
 
-                customs_uah = calculate_customs(year_val, engine_volume or 1.6, fuel_val or "petrol", price_val)
-                final_price = round(price_val * 41 + customs_uah, 2)
+                try:
+                    data = json.loads(script_tag.string)
+                    offer = data.get("offers", {})
+                    item = offer.get("itemOffered", {})
+
+                    identifier_val = item.get("identifier")
+                    brand_val = item.get("manufacturer")
+                    model_val = item.get("model")
+
+                    production_date = item.get("productionDate")
+                    if production_date:
+                        try:
+                            year_val = datetime.strptime(production_date, "%Y-%m-%d").year
+                        except ValueError:
+                            print("[WARN] Неможливо обробити productionDate:", production_date)
+
+                    mileage_val = safe_int(item.get("mileageFromOdometer", {}).get("value"))
+                    fuel_val = item.get("vehicleEngine", [{}])[0].get("fuelType")
+                    price_val = safe_float(offer.get("price"))
+                    transmission_val = item.get("vehicleTransmission")
+                    drive_val = item.get("driveWheelConfiguration")
+                    engine_volume = safe_float(item.get("vehicleEngine", [{}])[0].get("engineDisplacement", {}).get("value"))
+                    country_val = offer.get("offeredBy", {}).get("address", {}).get("addressCountry")
+                    body_val = item.get("bodyType")
+
+                except Exception as e:
+                    print("[ERROR] Неможливо обробити JSON структуру:", e)
+                    continue
+
+
+                print(f"{year_val} {engine_volume} {fuel_val} {price_val}")
+                customs_uah = calculate_customs(year_val, engine_volume, fuel_val, price_val)
+                final_price = round(price_val * eur_rate + customs_uah, 2)
 
                 car_data = {
                     "identifier": identifier_val,
@@ -140,10 +186,10 @@ def parse_autoscout24(brand=None, model=None, year_from=None, year_to=None,
                     "source": "AutoScout24"
                 }
 
-                print("[DEBUG] Saving car:", car_data)
+                print("[DEBUG] Збереження авто:", car_data)
                 save_car_to_db(car_data)
 
             except Exception as e:
-                print("[ERROR] Failed to process listing:", e)
+                print(f"[ERROR] Помилка при обробці авто: {e}")
 
         page += 1
